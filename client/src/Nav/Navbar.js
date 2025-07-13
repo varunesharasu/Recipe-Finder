46717711c5e6f2ad8190c039d565f90a60d61b33
VaruneshT <tvarunesharasu@gmail.com>
2025-07-13 21:50:45 +0530

    Updated Navbar.js

--- client/src/Nav/Navbar.js
+++ client/src/Nav/Navbar.js
@@ -87,7 +87,7 @@ const NavBar = () => {
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                   <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
                 </svg>
-                Admin
+                ADMIN
               </a>
             </li>
           )}
